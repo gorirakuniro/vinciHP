43fb873b3a00b33a22d75876493614a7f2b6b5a0
gorirakuniro <iwigenki0926@gmail.com>
2021-04-18 22:36:55 +0900

    workitemのUIいい具合に作ったお

--- js/main.js
+++ js/main.js
@@ -70,3 +70,16 @@ window.onload = function () {
 	// Add .loaded to .loading
 	spinner.classList.add('loaded');
 };
+
+//upper js
+const upper = document.querySelector('.up_btn');
+const wright = document.querySelector('.workitem__right');
+upper.addEventListener('click', () => {
+	wright.classList.toggle('upper');
+	const hsup = wright.classList.contains('upper');
+	if (hsup) {
+		upper.innerHTML = '↓';
+	} else {
+		upper.innerHTML = '↑';
+	}
+});
